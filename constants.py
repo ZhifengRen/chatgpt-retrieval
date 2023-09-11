22b8a514a0de89ab7b08144fa58614720a33604f
zhifengren <zhifengren@126.com>
2023-09-11 00:23:01 -0700

    change1

---
+++ constants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = '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'
