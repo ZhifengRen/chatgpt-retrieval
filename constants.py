d378e9b6a1e4d5592f5f1a588627a795fc8fb456
sidneyren <china.zhifengre@gmail.com>
2023-09-21 10:05:32 +0800

    ...

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
 #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
-APIKEY = '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'
+APIKEY = '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'
 NEWSAPI = 'fb12fb5253154c66b8bbe1da83d30139'
